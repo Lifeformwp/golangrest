152a939bb44d21c6e2e4641049a893dd054c4215
Serhii Kondratiuk <vielon.indie@yandex.ru>
2018-02-07 23:13:37 +0200

    Added structure

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"io/ioutil"
 )
+
+type Page struct {
+    Title string
+    Body []byte
+}
 
 func main() {
 	var router = mux.NewRouter()
-	router.HandleFunc("/check", check).Methods("GET")
-	router.HandleFunc("/new", handle).Methods("GET")
+	router.HandleFunc("/check", save).Methods("GET")
+	router.HandleFunc("/new", load).Methods("GET")
 	router.HandleFunc("/heythere/{msg}", view).Methods("GET")
 
 	fmt.Println("Hello this is server!")
@@ -34,4 +40,18 @@ func view(w http.ResponseWriter, r *http.Request) {
 	message := vars["msg"]
 
 	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+
+func save(w http.ResponseWriter, r *http.Request) {
+    p := &Page{Title: "Hello world", Body: []byte("This is our body-text")}
+    filename := p.Title + ".txt"
+    ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func load(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+    filename := vars.Get("title") + ".txt"
+    body, _ := ioutil.ReadFile(filename)
+
+    json.NewEncoder(w).Encode(&Page{Title: filename, Body: body})
 }
